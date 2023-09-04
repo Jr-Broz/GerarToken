b840a67197fc77b9d87b871fb28f4076fb91c1fa
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-04 18:42:50 -0300

    Coloquei o código um pouco mais "limpo"

--- Program.cs
+++ Program.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 namespace GerarToken{
-
 public class Program{
 
 public static void Main(string[] agrs){
 
-
 GerarToken gerando = new GerarToken();
-
-
 gerando.CriarToken();
 
 Console.WriteLine(gerando);
 
     }
-}
-}
+  }
+ }
